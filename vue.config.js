4c2c7295a60d7ae36e6df9cdf87e6be7387a5009
juanjuarez99 <juarez.castro.juan1cv1@gmail.com>
2022-03-04 06:32:15 -0600

    Cha :( La ruta no se dejo

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
 	transpileDependencies: true,
-	publicPath: "https://juanjuarez99.github.io/Proyecto-General-Beta/",
+	publicPath: "",
 });

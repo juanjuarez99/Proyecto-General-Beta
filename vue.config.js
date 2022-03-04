3dbc4a6315c867267dc9300a1ac40a05af98c4bf
juanjuarez99 <juarez.castro.juan1cv1@gmail.com>
2022-03-04 06:29:27 -0600

    Ruta camate pofavo

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
 	transpileDependencies: true,
-	publicPath: "./",
+	publicPath: "https://juanjuarez99.github.io/Proyecto-General-Beta/",
 });

62b7dfb6989e8c8150d33b9124636bb396954f93
marea <m.arevalo.aragon@gmail.com>
2022-03-28 00:18:38 -0600

    Cambia puerto predeterminado

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const serveStatic = require("serve-static");
 const v1Router = require("./api/v1/rutas");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 80;
 
 app.use(require("cors")());
 app.use(serveStatic("docs", { index: ["index.html"] }));

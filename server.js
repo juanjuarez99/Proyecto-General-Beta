f5387c730d1ebd7f55ab0bb9c26c9552bddfaffc
marea <m.arevalo.aragon@gmail.com>
2022-03-28 00:14:47 -0600

    Añade router para api

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const serveStatic = require("serve-static");
 
+const v1Router = require("./api/v1/rutas");
+
 const app = express();
 const port = 3000;
 
 app.use(require("cors")());
 app.use(serveStatic("docs", { index: ["index.html"] }));
+
+app.use("/api/v1/", v1Router);
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);

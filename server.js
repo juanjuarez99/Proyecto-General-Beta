d2d7467a731c23efade923e7ac0c085634d8f9c7
marea <m.arevalo.aragon@gmail.com>
2022-03-27 23:40:44 -0600

    Permite servir el directorio docs

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-import express from "express";
+const express = require("express");
+const serveStatic = require("serve-static");
 
 const app = express();
 const port = 3000;
 
-app.use("/", express.static("/public"));
+app.use(require("cors")());
+app.use(serveStatic("docs", { index: ["index.html"] }));
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);

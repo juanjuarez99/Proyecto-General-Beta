1c7c5e16138d03046a538423da7dd310bc89b9e0
marea <m.arevalo.aragon@gmail.com>
2022-03-30 16:43:58 -0600

    Añade funciones con la base de datos

--- api/v1/rutas/usuarios/controlador.js
+++ api/v1/rutas/usuarios/controlador.js
@@ -1,9 +1,44 @@
-exports.get = (req, res) => {
-	res.send("Hi user from controller");
+const bd = require("../../basedatos");
+
+exports.get = async (req, res) => {
+	try {
+		await bd.connect();
+		const respuesta = await bd.db("cafeteria").collection("usuarios").find({});
+		const usuarios = await respuesta.toArray();
+		res.send(usuarios);
+		await bd.close();
+	} catch (error) {
+		res.send({ error });
+	}
 };
 
-exports.post = (req, res) => {
-	res.send("Hi user from controller");
+exports.post = async (req, res) => {
+	try {
+		await bd.connect();
+		const respuesta = await bd
+			.db("cafeteria")
+			.collection("usuarios")
+			.insertOne(req.body);
+		res.send(respuesta);
+		await bd.close();
+	} catch (error) {
+		res.send({ error });
+	}
+};
+
+exports.getOne = async (req, res) => {
+	try {
+		await bd.connect();
+		const respuesta = await bd
+			.db("cafeteria")
+			.collection("usuarios")
+			.findOne({ nombre: req.params.id });
+		console.log(respuesta);
+		res.send(respuesta);
+		await bd.close();
+	} catch (error) {
+		res.send({ error });
+	}
 };
 
 exports.put = (req, res) => {

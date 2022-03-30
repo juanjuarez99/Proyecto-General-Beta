f43246f0c73ef2b735dfb72ea11f64e1a6415f74
marea <m.arevalo.aragon@gmail.com>
2022-03-30 16:42:57 -0600

    Añade ruta para obtener solo un usuario

--- api/v1/rutas/usuarios/index.js
+++ api/v1/rutas/usuarios/index.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const router = new Router();
 
-const { get, post, put, del } = require("./controlador");
+const { get, post, getOne, put, del } = require("./controlador");
 
 router.get("/", get);
+router.get("/:id", getOne);
 router.post("/", post);
 router.put("/", put);
 router.delete("/", del);

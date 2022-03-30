46d4cdace577eb3b5f1f0b30cee1f891e4f97fc8
marea <m.arevalo.aragon@gmail.com>
2022-03-30 16:42:28 -0600

    Añade parser para datos en formato JSON

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 80;
 
 app.use(require("cors")());
+app.use(express.json());
 app.use(serveStatic("docs", { index: ["index.html"] }));
 
 app.use("/api/v1/", v1Router);

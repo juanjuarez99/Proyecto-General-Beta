9d00ed9e5a711cd21bcf9ad5f0c50780dc750905
marea <m.arevalo.aragon@gmail.com>
2022-03-31 02:39:13 -0600

    Añade ruta para disminuir stock

--- api/v1/rutas/productos/index.js
+++ api/v1/rutas/productos/index.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const router = new Router();
 
-const { get, post, getOne, put, del } = require("./controlador");
+const { get, post, getOne, put, menosUno, del } = require("./controlador");
 
 router.get("/", get);
 router.get("/:id", getOne);
 router.post("/", post);
 router.put("/:id", put);
+router.put("/:id/dec", menosUno);
 router.delete("/:id", del);
 
 module.exports = router;

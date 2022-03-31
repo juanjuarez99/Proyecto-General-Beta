9d0e0af357b9936e26377ce72558363236a348af
juanjuarez99 <72789546+juanjuarez99@users.noreply.github.com>
2022-03-31 04:26:42 -0600

    Update index.js

--- api/v1/basedatos/index.js
+++ api/v1/basedatos/index.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 const API_URI =
-	"mongodb+srv://marea:mongo123@cluster0.px8ls.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+	"mongodb+srv://Juan:080299@cluster0.tpowb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const cliente = new MongoClient(API_URI);
 
 module.exports = cliente;
